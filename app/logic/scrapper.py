20238960dc991113e0177e3132a9ea1c5974348b
sagnikde16 <sagnikde191@gmail.com>
2025-07-05 12:56:29 +0530

    Update scrapper.py

--- app/logic/scrapper.py
+++ app/logic/scrapper.py
@@ -1,6 +1,5 @@
 from scrape_leetcode import scrape_leetcode_problems
 from codeforces import scrape_codeforces_problems
-from codechef import scrape_codechef_problems
 from gfg import scrape_gfg_problems
 
 import json
@@ -13,7 +12,6 @@ def save_combined_problems(leetcode_limit=100, codeforces_limit=100, codechef_li
     all_problems = (
         scrape_leetcode_problems(leetcode_limit)
         + scrape_codeforces_problems(codeforces_limit)
-        + scrape_codechef_problems(codechef_limit)
         + scrape_gfg_problems(gfg_limit)
     )
 
